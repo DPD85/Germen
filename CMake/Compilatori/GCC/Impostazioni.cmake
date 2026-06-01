a01131dc2511dcd77baf623b7b134894b444d7ac
Dario Passet <dario.passet@gmail.com>
2026-06-01 18:34:31 +0200

    Impostazioni dei compilatori si applicano solo al mio codice e non più anche alle dipendenze.

--- CMake/Compilatori/GCC/Impostazioni.cmake
+++ CMake/Compilatori/GCC/Impostazioni.cmake
@@ -1,5 +1,24 @@
 ﻿# ----- Impostazioni specifiche per GCC -----
 
-message(STATUS "GCC: abilito posizione indipendente del codice")
-set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -fPIC")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIC")
+message(STATUS "GCC: abilitati quasi tutti gli avvisi.")
+message(STATUS "GCC: abilitati gli avvisi extra.")
+message(STATUS "GCC: abilitati gli avvisi relativi al rispetto rigoroso degli standard ISO C e C++.")
+message(STATUS "GCC: avvisa quando la dichiarazione di un tipo ne nasconde un'altra.")
+message(STATUS "GCC: avvisa quando una class ha funzioni virtuali ma non ha un distruttore virtuale.")
+message(STATUS "GCC debug: abilito le informazioni di debug complete.")
+message(STATUS "GCC debug: abilito le ottimizzazioni compatibili con le informazioni di debug.")
+message(STATUS "GCC release: abilitata una maggiore protezione verso errori che interessano lo stack.")
+
+add_library(ImpostazioniCompilatore INTERFACE)
+
+target_compile_options(ImpostazioniCompilatore
+    INTERFACE
+        -Wall
+        -Wextra
+        -Wpedantic
+        -Wshadow
+        -Wnon-virtual-dtor
+        $<$<CONFIG:Debug>: -g3>
+        $<$<CONFIG:Debug>: -Og>
+        $<$<CONFIG:Release>: -fstack-protector-strong>
+)
